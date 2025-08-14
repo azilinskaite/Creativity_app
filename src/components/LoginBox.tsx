5826581bd5a51b5923adc1721fd52f2c062b3e1d
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-08-14 10:03:57 +0200

    Install Storybook

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 export default function LoginBox() {
   return (
-    <div className="bg-white bg-opacity-80 p-6 rounded max-w-sm">
-      <h2 className="text-xl mb-4">Sign up / Log in</h2>
+    <div className="bg-white bg-opacity-80 p-6 border rounded-3xl max-w-sm">
       <input
         type="email"
         placeholder="Email"
-        className="w-full mb-3 p-2 border rounded"
+        className="w-full mb-3 p-2 border rounded-xl"
       />
+      <input
+        type="password"
+        placeholder="Password"
+        className="w-full mb-3 p-2 border rounded-xl"
+      />
+      <button type="submit">Log In</button>
+      <h3>If you&#39;re new here</h3>
+      <button type="button">Sign Up</button>
     </div>
   );
 }
