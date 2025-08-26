f3df970f70afcf22379dd00ad94bee33b8f9bb42
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-08-26 15:04:30 +0300

    feat(navbar): add basic navigation menu layout without animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,41 @@
-import React from "react";
-import Link from 'next/link';
+"use client";
+import React, { useState } from "react";
+import Hamburger from "hamburger-react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <nav className="bg-white p-4">
-      <ul className="flex flex-col md:flex-row gap-4">
-        <li>
+    <nav className="bg-white w-full px-[2rem] py-[1rem] flex flex-col items-center md:flex-row md:justify-between md:items-center">
+      <div className="flex w-full flex-row justify-between">
+        <Image
+          src="/Ripple_logo.png"
+          alt="Site Logo"
+          width={80}
+          height={33}
+          style={{ width: "80px", height: "33px", objectFit: "contain" }}
+        />
+        <div className="md:hidden">
+          <Hamburger
+            size={24}
+            // toggled={isMenuOpen}
+            // toggle={setIsMenuOpen}
+          />
+        </div>
+      </div>
+
+      <ul className="w-full flex flex-col gap-[1rem] md:flex-row md:justify-end md:gap-[2rem]">
+        <hr className="border-1 border-black mt-[0.5rem] md:hidden" />
+        <li className="transition-[1s] md:hover:-rotate-15">
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className="transition-[1s] md:hover:-rotate-15">
           <Link href="/profile">Profile</Link>
         </li>
-        <li>
+        <li className="transition-[1s] md:hover:-rotate-15">
           <Link href="/challenges">Challenges</Link>
         </li>
       </ul>
