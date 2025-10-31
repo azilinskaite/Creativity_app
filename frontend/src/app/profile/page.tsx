f98168d9651aa1beca9b6f935ae57cf4532c8b09
Adele Zilinskaite <adele.zilinskaite@umain.com>
2025-10-31 15:19:20 +0100

    feat(profile page): update page to be server side

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import SectionHeader from "@/components/SectionHeader";
 import Moodboard from "@/components/Moodboard";
@@ -6,17 +5,14 @@ import Achievements from "@/components/Achievements";
 import Gallery from "@/components/Gallery";
 import { MoodboardProvider } from "@/components/Moodboard";
 import Footer from "@/components/Footer";
-import { useUser } from "@/hooks/useUser";
 import { RevealOnScroll } from "@/components/RevealOnScroll";
 
 export default function ProfilePage() {
-  const user = useUser();
-  const userName = user?.displayName ?? "user";
 
   return (
     <div>
       <MoodboardProvider>
-        <SectionHeader bg="bg-[var(--bright-red)]">{`Hi ${userName}!`}</SectionHeader>
+        <SectionHeader bg="bg-[var(--bright-red)]" showUser/>
         <Moodboard />
         <SectionHeader>Achievements</SectionHeader>
         <Achievements />
